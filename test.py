2b367eb1d6cf5554d73baf78e0288b9c26abc9e7
Yohei Tamura <tamuhey@gmail.com>
2019-01-03 23:50:46 +0900

    cleanup

--- test.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app=Flask(__name__)
-@app.route("/",methods=["GET"])
-def test():
-    return "hello"
-from werkzeug.serving import run_simple
-run_simple("localhost",5000,app)
